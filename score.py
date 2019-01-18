534d3b5cf32eaf40a32f55c0e6d271c34b9b8967
ZX Pek <zhixuan.pek@outlook.com>
2019-01-18 09:26:28 +0000

    clean up script, add text

--- score.py
+++ score.py
@@ -15,8 +15,8 @@ def cleanLongLat(l):
     return(split)
 
 def scale(x):
-    s_mean = pd.Series([1018.6536186748193, 1.7702220063430383, 859.1854052972942, 443661.2680362296, 269.6304465841881, 226.50495728449386, 233.054087259636, 515.351352895797, 1.361667476213606, 313.32132346638474, 0.18909111688905397, 13.01694334123832, 49.73222092059773, 5.895254150118575, 1.085973884968142, 0.7912226063601817, 63.65644732706649, 338.97779936569617, 78.55941598331381, 0.9553415811880339, 0.1976913626103603, 1.3385345295579019, 103.82176690763242],['Density_Overload', 'Abnormal_Flow_Rate', 'Heat_Flow', 'Asset_Integrity', 'Temperature_Differential', 'Volumetric_Flow_Rate', 'Tangential_Stress', 'Duct_Lengths_in_Units', 'Fault_in_last_Month', 'Avg_hours_in_Use', 'Pressure_Alarm', 'Inclination_Angle', 'Location_Fault_Code', 'Operating_Pressure_above_Normal', 'Vandalism_Reports', 'Compression_Ratio', 'Multiple_Connects', 'Water_Exposure_units', 'Humidity_Factor', 'Cathodic_Protection', 'Pressure_Class', 'Latitude', 'Longitude'])
-    s_var = pd.Series([2513529.9777650624, 1113.1883000570303, 1959427.1616068266, 148713873848.40573, 768178.5282640787, 77121.39970541374, 80938.43409944583, 369590.581359321, 10.430767846333982, 159138.84074506673, 0.2866048056581768, 251.76847112977725, 6363.065443013388, 56.590319495798596, 112.62690166816, 0.7542563456388259, 18409.98951720073, 403728.7554076676, 3777.931544501477, 0.042665263483529926, 0.15861401971831993, 0.0018770140014010134, 0.006205724168020728],['Density_Overload', 'Abnormal_Flow_Rate', 'Heat_Flow', 'Asset_Integrity', 'Temperature_Differential', 'Volumetric_Flow_Rate', 'Tangential_Stress', 'Duct_Lengths_in_Units', 'Fault_in_last_Month', 'Avg_hours_in_Use', 'Pressure_Alarm', 'Inclination_Angle', 'Location_Fault_Code', 'Operating_Pressure_above_Normal', 'Vandalism_Reports', 'Compression_Ratio', 'Multiple_Connects', 'Water_Exposure_units', 'Humidity_Factor', 'Cathodic_Protection', 'Pressure_Class', 'Latitude', 'Longitude'])
+    s_mean = pd.Series([1018.3124861106702, 1.7504047747547542, 856.766437029747, 443319.28505666845, 267.7161497190387, 226.50373027715165, 233.13035334455063, 516.0932410552716, 1.3675354773167403, 313.3358201847678, 0.1875932569287914, 13.010889234578876, 49.57471665767167, 5.892504523953141, 1.088415505254135, 0.7893583923299152, 63.58341534651894, 338.8552969935553, 78.57674846820534, 0.9547604685863044, 0.19664116321153052, 1.3387101177814704, 103.82191688625682],['Density_Overload', 'Abnormal_Flow_Rate', 'Heat_Flow', 'Asset_Integrity', 'Temperature_Differential', 'Volumetric_Flow_Rate', 'Tangential_Stress', 'Duct_Lengths_in_Units', 'Fault_in_last_Month', 'Avg_hours_in_Use', 'Pressure_Alarm', 'Inclination_Angle', 'Location_Fault_Code', 'Operating_Pressure_above_Normal', 'Vandalism_Reports', 'Compression_Ratio', 'Multiple_Connects', 'Water_Exposure_units', 'Humidity_Factor', 'Cathodic_Protection', 'Pressure_Class', 'Latitude', 'Longitude'])
+    s_var = pd.Series([2517027.6315063024, 1065.216575095123, 1954634.0917101507, 148756206721.0674, 764132.956667073, 76893.30307675204, 80870.7758832055, 370331.0477856238, 10.621120127599418, 158812.33683707696, 0.2865106179695057, 251.13585830824238, 6367.112289805666, 56.3985656333141, 115.93951243950882, 0.7537428704395421, 18527.598183252845, 404949.04503282363, 3777.5525179547, 0.04319428750190964, 0.15797843148983523, 0.0018802365891634565, 0.006215126624172454],['Density_Overload', 'Abnormal_Flow_Rate', 'Heat_Flow', 'Asset_Integrity', 'Temperature_Differential', 'Volumetric_Flow_Rate', 'Tangential_Stress', 'Duct_Lengths_in_Units', 'Fault_in_last_Month', 'Avg_hours_in_Use', 'Pressure_Alarm', 'Inclination_Angle', 'Location_Fault_Code', 'Operating_Pressure_above_Normal', 'Vandalism_Reports', 'Compression_Ratio', 'Multiple_Connects', 'Water_Exposure_units', 'Humidity_Factor', 'Cathodic_Protection', 'Pressure_Class', 'Latitude', 'Longitude'])
     
     result = (x - s_mean) / np.sqrt(s_var)
     return (result)
@@ -26,7 +26,7 @@ def init():
     global model
     global pca_transform
     
-    model_id = "AutoMLabe634baabest"
+    model_id = "AutoML057e77cdebest"
     model_path = Model.get_model_path(model_name = model_id)
     # deserialize the model file back into a sklearn model
     model = joblib.load(model_path)
@@ -40,7 +40,6 @@ def run(rawdata):
         df['Latitude'] = cleanLongLat(df['Latitude'])
         df['Longitude'] = cleanLongLat(df['Longitude'])
         df.drop(['Machine_ID', 'District'], axis=1, inplace=True)
-        df.drop('Failure_NextHour', axis = 1, inplace=True)
         df = df.apply(pd.to_numeric)
         data = pca_transform.transform(scale(df))[:,:10]
         result = model.predict(data)
